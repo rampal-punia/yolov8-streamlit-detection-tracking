b7e31fa6568a17c8ce2117ff7993f639076c69b8
Coding Mantras <rampalsinghpunia@gmail.com>
2023-05-07 19:20:09 +0530

    Update tracking with yolov8

--- helper.py
+++ helper.py
@@ -4,7 +4,6 @@ import cv2
 import pafy
 
 import settings
-import tracker
 
 
 def load_model(model_path):
@@ -21,13 +20,16 @@ def load_model(model_path):
     return model
 
 
-def display_tracker_option():
+def display_tracker_options():
     display_tracker = st.radio("Display Tracker", ('Yes', 'No'))
     is_display_tracker = True if display_tracker == 'Yes' else False
-    return is_display_tracker
+    if is_display_tracker:
+        tracker_type = st.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
+        return is_display_tracker, tracker_type
+    return is_display_tracker, None
 
 
-def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None):
+def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None, tracker=None):
     """
     Display the detected objects on a video frame using the YOLOv8 model.
 
@@ -45,13 +47,12 @@ def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=N
     # Resize the image to a standard size
     image = cv2.resize(image, (720, int(720*(9/16))))
 
-    # Predict the objects in the image using the YOLOv8 model
-    res = model.predict(image, conf=conf)
-    result_tensor = res[0].boxes
-
     # Display object tracking, if specified
     if is_display_tracking:
-        tracker._display_detected_tracks(result_tensor.data, image)
+        res = model.track(image, conf=conf, persist=True, tracker=tracker)
+    else:
+        # Predict the objects in the image using the YOLOv8 model
+        res = model.predict(image, conf=conf)
 
     # # Plot the detected objects on the video frame
     res_plotted = res[0].plot()
@@ -78,7 +79,7 @@ def play_youtube_video(conf, model):
     """
     source_youtube = st.sidebar.text_input("YouTube Video url")
 
-    is_display_tracker = display_tracker_option()
+    is_display_tracker, tracker = display_tracker_options()
 
     if st.sidebar.button('Detect Objects'):
         try:
@@ -93,7 +94,8 @@ def play_youtube_video(conf, model):
                                              model,
                                              st_frame,
                                              image,
-                                             is_display_tracker
+                                             is_display_tracker,
+                                             tracker
                                              )
                 else:
                     vid_cap.release()
@@ -117,7 +119,7 @@ def play_rtsp_stream(conf, model):
         None
     """
     source_rtsp = st.sidebar.text_input("rtsp stream url")
-    is_display_tracker = display_tracker_option()
+    is_display_tracker, tracker = display_tracker_options()
     if st.sidebar.button('Detect Objects'):
         try:
             vid_cap = cv2.VideoCapture(source_rtsp)
@@ -129,7 +131,8 @@ def play_rtsp_stream(conf, model):
                                              model,
                                              st_frame,
                                              image,
-                                             is_display_tracker
+                                             is_display_tracker,
+                                             tracker
                                              )
                 else:
                     vid_cap.release()
@@ -153,7 +156,7 @@ def play_webcam(conf, model):
         None
     """
     source_webcam = settings.WEBCAM_PATH
-    is_display_tracker = display_tracker_option()
+    is_display_tracker, tracker = display_tracker_options()
     if st.sidebar.button('Detect Objects'):
         try:
             vid_cap = cv2.VideoCapture(source_webcam)
@@ -165,7 +168,8 @@ def play_webcam(conf, model):
                                              model,
                                              st_frame,
                                              image,
-                                             is_display_tracker
+                                             is_display_tracker,
+                                             tracker,
                                              )
                 else:
                     vid_cap.release()
@@ -191,7 +195,7 @@ def play_stored_video(conf, model):
     source_vid = st.sidebar.selectbox(
         "Choose a video...", settings.VIDEOS_DICT.keys())
 
-    is_display_tracker = display_tracker_option()
+    is_display_tracker, tracker = display_tracker_options()
 
     with open(settings.VIDEOS_DICT.get(source_vid), 'rb') as video_file:
         video_bytes = video_file.read()
@@ -210,7 +214,8 @@ def play_stored_video(conf, model):
                                              model,
                                              st_frame,
                                              image,
-                                             is_display_tracker
+                                             is_display_tracker,
+                                             tracker
                                              )
                 else:
                     vid_cap.release()
