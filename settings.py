1c4e696178ce04e8d552ec55df534c1753c00c5a
Coding Mantras <rampalsinghpunia@gmail.com>
2023-09-09 15:11:31 +0530

    Add sample videos

--- settings.py
+++ settings.py
@@ -33,14 +33,10 @@ VIDEO_DIR = ROOT / 'videos'
 VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
 VIDEO_2_PATH = VIDEO_DIR / 'video_2.mp4'
 VIDEO_3_PATH = VIDEO_DIR / 'video_3.mp4'
-VIDEO_4_PATH = VIDEO_DIR / 'video_4.mp4'
-VIDEO_5_PATH = VIDEO_DIR / 'video_5.mp4'
 VIDEOS_DICT = {
     'video_1': VIDEO_1_PATH,
     'video_2': VIDEO_2_PATH,
     'video_3': VIDEO_3_PATH,
-    'video_4': VIDEO_4_PATH,
-    'video_5': VIDEO_5_PATH,
 }
 
 # ML Model config
