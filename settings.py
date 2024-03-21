6349f8c29aaa12a6aa0a50e6953e0fa8c85ebb39
Coding Mantras <rampalsinghpunia@gmail.com>
2024-03-21 22:57:27 +0530

    update settings

--- settings.py
+++ settings.py
@@ -2,17 +2,14 @@ from pathlib import Path
 import sys
 
 # Get the absolute path of the current file
-file_path = Path(__file__).resolve()
-
+FILE = Path(__file__).resolve()
 # Get the parent directory of the current file
-root_path = file_path.parent
-
+ROOT = FILE.parent
 # Add the root path to the sys.path list if it is not already there
-if root_path not in sys.path:
-    sys.path.append(str(root_path))
-
+if ROOT not in sys.path:
+    sys.path.append(str(ROOT))
 # Get the relative path of the root directory with respect to the current working directory
-ROOT = root_path.relative_to(Path.cwd())
+ROOT = ROOT.relative_to(Path.cwd())
 
 # Sources
 IMAGE = 'Image'
@@ -30,13 +27,10 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
-VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
-VIDEO_2_PATH = VIDEO_DIR / 'video_2.mp4'
-VIDEO_3_PATH = VIDEO_DIR / 'video_3.mp4'
 VIDEOS_DICT = {
-    'video_1': VIDEO_1_PATH,
-    'video_2': VIDEO_2_PATH,
-    'video_3': VIDEO_3_PATH,
+    'video_1': VIDEO_DIR / 'video_1.mp4',
+    'video_2': VIDEO_DIR / 'video_2.mp4',
+    'video_3': VIDEO_DIR / 'video_3.mp4',
 }
 
 # ML Model config
